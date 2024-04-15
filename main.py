e59753f85a38f530533ce713f863b91abb6e06f8
coolestKitten <sepcaro2@gmail.com>
2024-04-15 13:15:41 -0600

    Implemented while loop to navigate the transition matrix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pysine as snd
 import random as rm
+import numpy as np
 
 ## Frequenciy of each note in Hz
 note1 = 440.0
@@ -28,7 +29,176 @@ TransitionMatrix = [
     [0.1, 0.1, 0.2, 0.1, 0.1, 0.3, 0.1]
 ]
 
-
+while (tempControl <= 21):
+    if curr == 0:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            pass
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+        
+    elif curr == 1:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            pass
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+        
+    elif curr == 2:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            pass
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+        
+    elif curr == 3:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            pass
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+                 
+    elif curr == 4:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            pass
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+          
+    elif curr == 5:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            pass
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            curr = next
+            
+    elif curr == 6:
+        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
+        if next == 0:
+            snd.sine(frequency=note1, duration=0.3) 
+            curr = next
+        if next == 1:
+            snd.sine(frequency=note2, duration=0.3) 
+            curr = next
+        if next == 2:
+            snd.sine(frequency=note3, duration=0.3) 
+            curr = next
+        if next == 3:
+            snd.sine(frequency=note4, duration=0.3) 
+            curr = next
+        if next == 4:
+            snd.sine(frequency=note5, duration=0.3) 
+            curr = next
+        if next == 5:
+            snd.sine(frequency=note6, duration=0.3) 
+            curr = next
+        if next == 6:
+            snd.sine(frequency=note7, duration=0.3) 
+            pass
+         
+    tempControl += 1
 
 
 
