911b30c4691afa80a51be0cfca5f07ba2fb8083f
coolestKitten <sepcaro2@gmail.com>
2024-04-15 18:38:45 -0600

    Simplified implementation to decrease execution time

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import pysine as snd
+import random as rnd
 import numpy as np
 
 ## Frequenciy of each note in Hz
-note1 = 440.0
-note2 = 660.0
-note3 = 990.0
-note4 = 1485.0
-note5 = 2227.5
-note6 = 3341.25
-note7 = 5011.875
+notes = [440.0, 660.0, 990.0, 1485.0, 2227.5, 3341.25, 5011.875]
 
 ## Control variables for movement 
-curr = 0
+curr = rnd.randint(0,6) ## Initializes current node as a random integer between 1 and 6
 next = 0
 
 ## Controls how many times the loop will occur
@@ -26,175 +21,23 @@ TransitionMatrix = [
     [0.1, 0.2, 0.1, 0.3, 0.1, 0.1, 0.1],
     [0.3, 0.1, 0.1, 0.1, 0.2, 0.1, 0.1],
     [0.1, 0.1, 0.2, 0.1, 0.1, 0.3, 0.1]
-]
-
-while (tempControl <= 21):
-    if curr == 0:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            pass
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-        
-    elif curr == 1:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            pass
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-        
-    elif curr == 2:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            pass
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-        
-    elif curr == 3:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            pass
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-                 
-    elif curr == 4:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            pass
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-          
-    elif curr == 5:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            pass
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            curr = next
-            
-    elif curr == 6:
-        next = np.random.choice(7, replace=True, p=TransitionMatrix[curr])
-        if next == 0:
-            snd.sine(frequency=note1, duration=0.3) 
-            curr = next
-        if next == 1:
-            snd.sine(frequency=note2, duration=0.3) 
-            curr = next
-        if next == 2:
-            snd.sine(frequency=note3, duration=0.3) 
-            curr = next
-        if next == 3:
-            snd.sine(frequency=note4, duration=0.3) 
-            curr = next
-        if next == 4:
-            snd.sine(frequency=note5, duration=0.3) 
-            curr = next
-        if next == 5:
-            snd.sine(frequency=note6, duration=0.3) 
-            curr = next
-        if next == 6:
-            snd.sine(frequency=note7, duration=0.3) 
-            pass
-         
+]   
+    
+while(tempControl <= 21):
+    ## Chooses an element from the current node's transition array based on the 
+    next = np.random.choice(7, replace=True, p=TransitionMatrix[curr]) 
+    ## Plays a sound corresponding to the current note
+    snd.sine(frequency=notes[curr], duration=0.3)
+    ## If the note transition corresponds to itself, do nothing
+    ## If the transition is to a different note, set the current note's index to the next one
+    if curr == next:
+        pass
+    else:
+        curr = next
+    
+    ## Increase the counter cycle
     tempControl += 1
+        
+         
+        
+    
